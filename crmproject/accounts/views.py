3dfa84156d2559cb6dd7f60db4a3e80a921d8b3e
bhupathichintu <bhupathidaripalli@gmail.com>
2020-03-06 16:09:37 +0530

    crmpro update

--- crmproject/accounts/views.py
+++ crmproject/accounts/views.py
@@ -6,7 +6,7 @@ def home(request):
     orders =  Orders.objects.all()
     total_orders = len(orders)
     delivery_orders = orders.filter(status='Delivered').count()
-    pending_orders = orders.filter(status='Pending ').count()
+    pending_orders = orders.filter(status='Pending').count()
     outfordelivery_orders = orders.filter(status='Outfordelivery').count()
 
     context = {
@@ -22,7 +22,17 @@ def home(request):
     return render(request, 'accounts/dashboard.html',context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Products.objects.all()
+    context = {'products':products}
+    return render(request, 'accounts/products.html',context)
 
 def orders(request):
     return render(request, 'accounts/orders.html')
+def customer(request,pk):
+    customer = Customers.objects.get(id=pk)
+    context = {'customer':customer}
+    return render(request,'accounts/customer.html',context)
+def all_customers(request):
+    customers = Customers.objects.all()
+    context = {'customers':customers}
+    return render(request,'accounts/customers.html',context)
